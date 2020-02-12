440e15553e914f441ef16317add92dd279f43a06
echo 'hello world <ssmyth1071@gmail.com>
2020-02-12 10:53:26 -0500

    create Team model and add test data

--- elo/models.py
+++ elo/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Team(models.Model):
+    abbreviation = models.CharField(max_length=4)
+    city = models.CharField(max_length=70)
+    nickname = models.CharField(max_length=70)
+    colors = models.CharField(max_length=10)
+    ## Add the foreign key from the ratings model
+    #elo_rating = models.ForeignKey(Rating)
+    def __str__(self):
+        return self.abbreviation
