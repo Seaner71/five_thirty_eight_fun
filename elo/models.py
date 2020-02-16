5c7d77e8d48a9a6ff7d14286fc293db55d93b559
echo 'hello world <ssmyth1071@gmail.com>
2020-02-16 15:57:44 -0500

    updated Team model to include league, conference and division. need to migrate

--- elo/models.py
+++ elo/models.py
@@ -7,7 +7,15 @@ class Team(models.Model):
     city = models.CharField(max_length=70)
     nickname = models.CharField(max_length=70)
     colors = models.CharField(max_length=10)
+    league = models.CharField(max_length=5)
+    conference = models.CharField(max_length=30)
+    division = models.CharField(max_length=30)
     ## Add the foreign key from the ratings model
-    #elo_rating = models.ForeignKey(Rating)
+    #elo_rating = models.ForeignKey(EloRating)
     def __str__(self):
         return self.abbreviation
+''' Figure out connecting the Team and EloRating field
+class EloRating(models.Model):
+    abbreviation = models.ForeignKey(Team)
+    rating = models.IntegerField()
+'''
